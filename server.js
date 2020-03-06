d109b06953976b75a47a73508545592bdedeab66
Josiah Sayers <josiah.sayers15@gmail.com>
2020-03-05 21:21:51 -0500

    changes how static files are served

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const randomRouter = require('./controllers/random');
 const port = 3000;
 
 app.use(cors());
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use('/random', randomRouter);
-
-app.get('/', (req, res) => {
-  res.render('/public/index.html');
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
